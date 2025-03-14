e38481027bb961db0dd1cea31b0bed0d69b22748
kangaroo <tgkhang22@clc.fitus.edu.vn>
2025-03-14 16:29:17 +0700

    2

--- App.xaml.cs
+++ App.xaml.cs
@@ -34,7 +34,7 @@ namespace POS_For_Small_Shop
         public App()
         {
             this.InitializeComponent();
-            Service.AddKeyedSingleton<IDao, MockRepository>(); // TextDao, PostgresDao, SqlServerDao, RestDao, GraphQLDao,APIDao
+            Service.AddKeyedSingleton<IDao, MockDao>(); // TextDao, PostgresDao, SqlServerDao, RestDao, GraphQLDao,APIDao
         }
 
         /// <summary>

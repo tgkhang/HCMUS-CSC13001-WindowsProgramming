ef7f8ebcdfae2a5c0e118883a79302985b40d777
khang <tgkhang22@clc.fitus.edu.vn>
2025-03-28 22:40:39 +0700

    [m] Fixing file compose

--- API/knexfile.js
+++ API/knexfile.js
@@ -10,9 +10,13 @@ module.exports = {
     connection: {
       host: process.env.DB_HOST || 'localhost',
       port: process.env.DB_PORT || 5432,
-      database: `${process.env.POSTGRES_DB}`,
-      user:     `${process.env.POSTGRES_USER}`,
-      password: `${process.env.POSTGRES_PASSWORD}`
+      database: process.env.DB_NAME || 'database1',
+      user: process.env.DB_USER || 'postgres',
+      password: process.env.DB_PASSWORD || '1234'
+    },
+    pool: {
+      min: 2,
+      max: 10
     },
     migrations: {
       tableName: 'knex_migrations',

6a6e12fb822d1e725c0f263e6d48ae72bb034efe
khang <tgkhang22@clc.fitus.edu.vn>
2025-03-28 22:15:58 +0700

    docker

--- API/knexfile.js
+++ API/knexfile.js
@@ -8,12 +8,18 @@ module.exports = {
   development: {
     client: 'postgresql',
     connection: {
+      host: process.env.DB_HOST || 'localhost',
+      port: process.env.DB_PORT || 5432,
       database: `${process.env.POSTGRES_DB}`,
       user:     `${process.env.POSTGRES_USER}`,
       password: `${process.env.POSTGRES_PASSWORD}`
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: 'knex_migrations',
+      directory: './migrations'
+    },
+    seeds: {
+      directory: './seeds'
     }
   },
 

0eb9a9535f0fa0e8969ad0ad236aab3e2897f2a7
khang <tgkhang22@clc.fitus.edu.vn>
2025-04-04 17:21:31 +0700

    anon

---
+++ API/migrations/20250404101942_add_anon_role_for_postgrest.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function(knex) {
+    await knex.raw(`
+        create role anon nologin;
+
+        grant usage on schema public to anon;
+        grant select on public.category to anon;
+    `);
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function(knex) {
+    await knex.raw(`
+        drop role anon;
+    `);
+};

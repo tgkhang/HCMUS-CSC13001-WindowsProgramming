35097b5a14da05f2ef84e30c75449f816ce6f0fc
khang <tgkhang22@clc.fitus.edu.vn>
2025-04-06 01:13:56 +0700

    new

---
+++ POS For Small Shop/Converters/CategoryIdToNameConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.UI.Xaml.Data;
+using POS_For_Small_Shop.Services;
+
+namespace POS_For_Small_Shop.Converters
+{
+    public class CategoryIdToNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is int categoryId)
+            {
+                var dao = Service.GetKeyedSingleton<IDao>();
+                var categories = dao.Categories.GetAll();
+                var category = categories.FirstOrDefault(c => c.CategoryID == categoryId);
+                return category?.Name ?? "Unknown";
+            }
+            return "Unknown";
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

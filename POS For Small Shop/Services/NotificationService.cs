bac4e6fbb93b72f9f3d4662d158ce3a34039b92c
unknown <nguyenhoangquochuy12122002@gmail.com>
2025-04-22 09:08:55 +0700

    Add Notification Service

---
+++ POS For Small Shop/Services/NotificationService.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using POS_For_Small_Shop.Views;
+using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS_For_Small_Shop.Services
+{
+
+    public class Notification
+    {
+        public string Message { get; set; }
+        public string Target { get; set; } // The target page to navigate to (PromotionPage, InventoryPage, etc.)
+        public string ItemName { get; set; } // The name of the item related to the notification
+    }
+
+    [AddINotifyPropertyChangedInterface]
+    public class NotificationService
+    {
+        public ObservableCollection<Notification> Notifications { get; set; } = new ObservableCollection<Notification>();
+
+        private Frame _frame; // Storage the frame to navigate to the page
+        private IDao _dao = Service.GetKeyedSingleton<IDao>(); // Storage the dao to access the database
+
+        public NotificationService(Frame frame)
+        {
+            _frame = frame;
+
+            // Load the notifications
+            LoadPromotionNotification();
+            LoadInventoryNotification();
+        }
+
+        // Load the notification of promotion
+        public void LoadPromotionNotification()
+        {
+            var promotions = _dao.Promotions.GetAll();
+            var today = DateTime.Today;
+
+            foreach (var promo in promotions)
+            {
+                if (promo.StartDate <= today && promo.EndDate >= today)
+                {
+                    Notifications.Add(new Notification
+                    {
+                        Message = $"Promotion \"{promo.PromoName}\" is active today.",
+                        Target = "Promotion",
+                        ItemName = promo.PromoName
+                    });
+                }
+
+                if (promo.EndDate == today)
+                {
+                    Notifications.Add(new Notification
+                    {
+                        Message = $"Promotion \"{promo.PromoName}\" expires today!",
+                        Target = "Promotion",
+                        ItemName = promo.PromoName
+                    });
+                }
+
+                if (promo.EndDate == today.AddDays(-1))
+                {
+                    Notifications.Add(new Notification
+                    {
+                        Message = $"Promotion \"{promo.PromoName}\" just expired yesterday.",
+                        Target = "Promotion",
+                        ItemName = promo.PromoName
+                    });
+                }
+            }
+        }
+
+        // Loadd the notification of inventory
+        public void LoadInventoryNotification()
+        {
+
+        }
+
+        public void GoToTarget(Notification notification)
+        {
+            Type? targetPage = notification.Target switch
+            {
+                "Promotion" => typeof(PromotionManagementPage),
+                "Inventory" => typeof(InventoryPage),
+                _ => null
+            };
+
+            if (targetPage != null)
+            {
+                _frame.Navigate(targetPage, notification.ItemName);
+            }
+        }
+    }
+}

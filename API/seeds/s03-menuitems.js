c1eb269c5d7c457eed757446f5d3cb5d96c225ee
unknown <nguyenhoangquochuy12122002@gmail.com>
2025-04-25 15:32:41 +0700

    Update Images for MenuItems

--- API/seeds/s03-menuitems.js
+++ API/seeds/s03-menuitems.js
@@ -10,33 +10,33 @@ exports.seed = async function(knex) {
   const commonImagePath = '/Assets/StoreLogo.png';
 
   await knex('menu_item').insert([
-    { name: 'Espresso', category_id: categoryMap['Hot Coffee'], selling_price: 35000, image_path: commonImagePath },
-    { name: 'Americano', category_id: categoryMap['Hot Coffee'], selling_price: 40000, image_path: commonImagePath },
-    { name: 'Cappuccino', category_id: categoryMap['Hot Coffee'], selling_price: 47500, image_path: commonImagePath },
-    { name: 'Latte', category_id: categoryMap['Hot Coffee'], selling_price: 45000, image_path: commonImagePath },
-    { name: 'Mocha', category_id: categoryMap['Hot Coffee'], selling_price: 50000, image_path: commonImagePath },
+    { name: 'Espresso', category_id: categoryMap['Hot Coffee'], selling_price: 35000, image_path: '/Assets/MenuItems/espresso.jpg' },
+    { name: 'Americano', category_id: categoryMap['Hot Coffee'], selling_price: 40000, image_path: '/Assets/MenuItems/americano.jpg' },
+    { name: 'Cappuccino', category_id: categoryMap['Hot Coffee'], selling_price: 47500, image_path: '/Assets/MenuItems/Cappuccino.jpg' },
+    { name: 'Latte', category_id: categoryMap['Hot Coffee'], selling_price: 45000, image_path: '/Assets/MenuItems/Latte.jpg' },
+    { name: 'Mocha', category_id: categoryMap['Hot Coffee'], selling_price: 50000, image_path: '/Assets/MenuItems/Mocha.jpg' },
 
-    { name: 'Iced Coffee', category_id: categoryMap['Cold Coffee'], selling_price: 42500, image_path: commonImagePath },
-    { name: 'Cold Brew', category_id: categoryMap['Cold Coffee'], selling_price: 47500, image_path: commonImagePath },
-    { name: 'Iced Latte', category_id: categoryMap['Cold Coffee'], selling_price: 50000, image_path: commonImagePath },
-    { name: 'Frappuccino', category_id: categoryMap['Cold Coffee'], selling_price: 55000, image_path: commonImagePath },
+    { name: 'Iced Coffee', category_id: categoryMap['Cold Coffee'], selling_price: 42500, image_path: '/Assets/MenuItems/Iced Coffee.jpg' },
+    { name: 'Cold Brew', category_id: categoryMap['Cold Coffee'], selling_price: 47500, image_path: '/Assets/MenuItems/Cold Brew.jpg' },
+    { name: 'Iced Latte', category_id: categoryMap['Cold Coffee'], selling_price: 50000, image_path: '/Assets/MenuItems/Iced Latte.jpg' },
+    { name: 'Frappuccino', category_id: categoryMap['Cold Coffee'], selling_price: 55000, image_path: '/Assets/MenuItems/Frappuccino.jpg' },
 
-    { name: 'Caramel Macchiato', category_id: categoryMap['Specialty Drinks'], selling_price: 55000, image_path: commonImagePath },
-    { name: 'Vanilla Bean Latte', category_id: categoryMap['Specialty Drinks'], selling_price: 57500, image_path: commonImagePath },
-    { name: 'Hazelnut Mocha', category_id: categoryMap['Specialty Drinks'], selling_price: 57500, image_path: commonImagePath },
+    { name: 'Caramel Macchiato', category_id: categoryMap['Specialty Drinks'], selling_price: 55000, image_path: '/Assets/MenuItems/Caramel Macchiato.jpg' },
+    { name: 'Vanilla Bean Latte', category_id: categoryMap['Specialty Drinks'], selling_price: 57500, image_path: '/Assets/MenuItems/Vanilla Bean Latte.jpg' },
+    { name: 'Hazelnut Mocha', category_id: categoryMap['Specialty Drinks'], selling_price: 57500, image_path: '/Assets/MenuItems/Hazelnut Mocha.jpg' },
 
-    { name: 'Green Tea', category_id: categoryMap['Tea'], selling_price: 37500, image_path: commonImagePath },
-    { name: 'Chai Latte', category_id: categoryMap['Tea'], selling_price: 45000, image_path: commonImagePath },
-    { name: 'Iced Tea', category_id: categoryMap['Tea'], selling_price: 37500, image_path: commonImagePath },
+    { name: 'Green Tea', category_id: categoryMap['Tea'], selling_price: 37500, image_path: '/Assets/MenuItems/Green Tea.jpg' },
+    { name: 'Chai Latte', category_id: categoryMap['Tea'], selling_price: 45000, image_path: '/Assets/MenuItems/Chai Latte.jpg' },
+    { name: 'Iced Tea', category_id: categoryMap['Tea'], selling_price: 37500, image_path: '/Assets/MenuItems/Iced Tea.jpg' },
 
-    { name: 'Chocolate Croissant', category_id: categoryMap['Pastries'], selling_price: 37500, image_path: commonImagePath },
-    { name: 'Blueberry Muffin', category_id: categoryMap['Pastries'], selling_price: 35000, image_path: commonImagePath },
-    { name: 'Cinnamon Roll', category_id: categoryMap['Pastries'], selling_price: 42500, image_path: commonImagePath },
+    { name: 'Chocolate Croissant', category_id: categoryMap['Pastries'], selling_price: 37500, image_path: '/Assets/MenuItems/Chocolate Croissant.jpg' },
+    { name: 'Blueberry Muffin', category_id: categoryMap['Pastries'], selling_price: 35000, image_path: '/Assets/MenuItems/Blueberry Muffin.jpg' },
+    { name: 'Cinnamon Roll', category_id: categoryMap['Pastries'], selling_price: 42500, image_path: '/Assets/MenuItems/Cinnamon Roll.jpg' },
 
-    { name: 'Turkey & Cheese', category_id: categoryMap['Sandwiches'], selling_price: 67500, image_path: commonImagePath },
-    { name: 'Veggie Wrap', category_id: categoryMap['Sandwiches'], selling_price: 65000, image_path: commonImagePath },
+    { name: 'Turkey & Cheese', category_id: categoryMap['Sandwiches'], selling_price: 67500, image_path: '/Assets/MenuItems/Turkey & Cheese.jpg' },
+    { name: 'Veggie Wrap', category_id: categoryMap['Sandwiches'], selling_price: 65000, image_path: '/Assets/MenuItems/Veggie Wrap.jpg' },
 
-    { name: 'Avocado Toast', category_id: categoryMap['Breakfast Items'], selling_price: 75000, image_path: commonImagePath },
-    { name: 'Breakfast Sandwich', category_id: categoryMap['Breakfast Items'], selling_price: 62500, image_path: commonImagePath }
+    { name: 'Avocado Toast', category_id: categoryMap['Breakfast Items'], selling_price: 75000, image_path: '/Assets/MenuItems/Avocado Toast.jpg' },
+    { name: 'Breakfast Sandwich', category_id: categoryMap['Breakfast Items'], selling_price: 62500, image_path: '/Assets/MenuItems/Breakfast Sandwich.jpg' }
   ]);
 };
